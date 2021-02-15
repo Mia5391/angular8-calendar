acf5282c67f0a820c328f1664e178201d3fb1b2f
Mia <mariakrasteva5391@gmail.com>
2021-02-15 00:05:09 -0300

    implement filter by Category and Tag

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,13 +7,25 @@ import { HttpClient } from "@angular/common/http";
 export class ApiService {
   private SERVER_URL = "https://timelyapp.time.ly/api/calendars/4243455/events";
 
-  private query = (startDate?:string, page?:number, perPage?:number) => {
-    return `${this.SERVER_URL}?start_date=${startDate}&page=${page}&per_page=${perPage}`
+  private eventQuery = (startDate?:string, page?:number, perPage?:number, categoryId?:number, tagId?:number) => {
+
+    const catString = categoryId ? `&categories=${categoryId}` : ""
+    const tagString = tagId ? `&tags=${tagId}` : ""
+
+    return `${this.SERVER_URL}?start_date=${startDate}&page=${page}&per_page=${perPage}${catString}${tagString}`
   }
 
+  private TAXONOMY_CATEGORY_QUERY = "https://timelyapp.time.ly/api/calendars/4243455/taxonomies?type=taxonomy_category&page=1&per_page=100"
+  private TAXONOMY_TAG_QUERY = "https://timelyapp.time.ly/api/calendars/4243455/taxonomies?type=taxonomy_tag&page=1&per_page=100"
   constructor(private httpClient: HttpClient) {}
 
-  public get(startDate?:string, page?:number, perPage?:number) {
-    return this.httpClient.get(this.query(startDate, page, perPage));
+  public getEvents(startDate?:string, page?:number, perPage?:number, categoryId?:number, tagId?:number) {
+    return this.httpClient.get(this.eventQuery(startDate, page, perPage, categoryId, tagId));
+  }
+  public getCategories(){
+    return this.httpClient.get(this.TAXONOMY_CATEGORY_QUERY)
+  }
+  public getTags(){
+    return this.httpClient.get(this.TAXONOMY_TAG_QUERY)
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,29 +16,48 @@ export class HomeComponent implements OnInit {
   pageSize = 20;
   pageRange = [];
   startDate = new FormControl(new Date());
-  
 
+  categories = [];
+  tags = [];
+  categoryId = null;
+  tagId = null;
 
-  constructor(private apiService: ApiService) {
-    
+  constructor(private apiService: ApiService) {}
+
+  refresh() {
+    this.apiService
+      .getEvents(
+        this.startDate.value.toISOString().split("T")[0],
+        this.page,
+        this.pageSize,
+        this.categoryId,
+        this.tagId
+      )
+      .subscribe((data: any) => {
+        console.log(data.data);
+        this.events = data.data.items;
+        this.totalEvents = data.data.total;
+        this.pages = Math.ceil(this.totalEvents / this.pageSize);
+        this.pageRange = Object.getOwnPropertyNames(
+          Array.from(new Array(this.pages))
+        ).slice(0, -1);
+        console.log(this.events);
+      });
   }
 
-  refresh(){
-
-    this.apiService.get(this.startDate.value.toISOString().split('T')[0], this.page, this.pageSize).subscribe((data: any) => {
-      console.log(data.data);
-      this.events = data.data.items;
-      this.totalEvents = data.data.total;
-      this.pages = Math.ceil(this.totalEvents/this.pageSize)
-      this.pageRange = Object.getOwnPropertyNames(Array.from(new Array(this.pages))).slice(0,-1);
-      console.log(this.events);
-    });
+  log(input:any){
+    console.log(input)
   }
 
   ngOnInit() {
+    this.apiService.getCategories().subscribe((data:any) => {
+      this.categories = data.data.items
+      
+    });
+    this.apiService.getTags(). subscribe((data:any) => {
+      this.tags = data.data.items
+      
+    });
     this.refresh();
-    
-
   }
-
 }
